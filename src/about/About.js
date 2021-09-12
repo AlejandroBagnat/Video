f5cae8c9b5338f8eb86ea2cc93610becb0f16c35
AlejandroBagnat <43849183+AlejandroBagnat@users.noreply.github.com>
2021-09-12 19:49:45 -0300

    inicializacion

---
+++ src/about/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./About.css";
+
+const About = () => {
+    return (
+        <div className="about-container">
+            <div className="about-desc">
+                <h3>Dejame decirte algo sobre mì</h3>
+            </div>
+            <div className="about-img">
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fncube.com%2Fblog%2Ffull-stack-developer-vs-mean-stack-developer&psig=AOvVaw21gRuZkpYaXEcz-BjamIrT&ust=1631058795468000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMCmlNbF6_ICFQAAAAAdAAAAABAj" alt="" />
+            </div>
+        </div>
+    );
+}
+export default About
